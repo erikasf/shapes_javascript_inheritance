c69a60d1c3a4763b6829f9a6dd7620eff3373782
erika <erika@erikasf.com>
2015-05-24 19:20:23 -0700

    Update square.js

--- src/square.js
+++ src/square.js
@@ -4,6 +4,43 @@ function Square(sideLength, color) {
   Shape.call(4, color);
   this.sideLength = sideLength;
 }
+var Shape = require("./shape");
+
+function Square(sideLength, color) {
+  Shape.call(this, 4, color);
+  this.sideLength = sideLength;
+}
+Square.prototype = Object.create(Shape.prototype);
+Square.prototype.constructor = Square;
+
+// TODO: Implement area, perimeter, draw and toString for Square.
+// DO NOT reimplement getRGB.  That should only have to be done once.
+
+Square.prototype = {
+	area: function(){
+		return this.sideLength * this.sideLength;
+	},
+	perimeter: function(){
+		return this.sideLength * 4;
+	},
+	draw: function(){
+		var picture = "";
+		for (var i = 0; i < this.sideLength; i++){
+			picture += "-";		
+		}
+		for (var j = 0; j < this.sideLength-1; j++){
+			picture += "\n|";
+			picture += " ";
+			picture += "|";
+		}
+		picture += "\n";
+		for (var k = 0; k < this.sideLength; k++){
+			picture += "-";		
+		}
+		return picture;	
+	}
+};
+module.exports = Square;
 
 // TODO: Implement area, perimeter, draw and toString for Square.
 // DO NOT reimplement getRGB.  That should only have to be done once.
