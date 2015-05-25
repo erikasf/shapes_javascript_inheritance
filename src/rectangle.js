8f530af1b8736c7916352d493c45f57c9598ef5e
erika <erika@erikasf.com>
2015-05-24 19:15:45 -0700

    Update rectangle.js

--- src/rectangle.js
+++ src/rectangle.js
@@ -3,7 +3,39 @@ var Shape = require("./shape");
 // Add the constructor for rectangle and the methods that
 // need to be overwritten.  Keep in mind that a rectangle
 // has more than just 1 side length.
-function Rectangle() { // TODO: THIS IS JUST A PLACE HOLDER, PLEASE CHANGE.
+function Rectangle(sideLength, sideWidth, color) { 
+	Shape.call(this, 4, color);
+	this.sideLength = sideLength;
+	this.sideWidth = sideWidth;
 }
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+Rectangle.prototype = {
+	area: function(){
+		return this.sideWidth * this.sideLength;
+	},
+	perimeter: function(){
+		return this.sideWidth*2 + this.sideLength*2;
+	},
+	draw: function(){
+	   var picture = "";
+	   for (var i = 0; i < this.sideWidth; i++){
+		    picture += "-";		
+	   }
+	   for (var j = 0; j < this.sideLength; j++){
+		    picture += "\n|";
+		    picture += " ";
+		    picture += "|";
+	   }
+  	    picture += "\n";
+	   for (var k = 0; k < this.sideWidth; k++){
+		    picture += "-";		
+	   }
+	return picture;	
+}
+};
+
+
 
 module.exports = Rectangle;
